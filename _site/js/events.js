801e12ac0c11e3b2752911518bcd6ef361c3b08e
Nicholas Gunner <support@getbeyondwords.com>
2018-01-24 13:02:48 -0700

    working on styles

--- _site/js/events.js
+++ _site/js/events.js
@@ -3,8 +3,8 @@
   var eventsAPI = "http://live-westfield-ny.pantheonsite.io/api/upcoming-events";
   $.getJSON(eventsAPI)
   .done(function( data ) {
-    for (i = 0; i < 10; i++) {
-      $( "#events" ).append(
+    for (i = 0; i < 5; i++) {
+      $( "#events-content" ).append(
         "<h2>" + data[i].title + "</h2>" +
         "<p>" + data[i].field_section + "</p>" +
         "<p class='lead'>" + data[i].field_date_time + "</p>" +
--- js/news.js
+++ js/news.js
@@ -3,13 +3,13 @@
   var eventsAPI = "http://live-westfield-ny.pantheonsite.io/api/latest-news";
   $.getJSON(eventsAPI)
   .done(function( data ) {
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 5; i++) {
       if (data[i].image.length > 1) {
         var eventImage = "<img src='http://live-westfield-ny.pantheonsite.io" + data[i].image + "'>"
       } else {
         var eventImage = "";
       }
-      $( "#news" ).append(
+      $( "#news-content" ).append(
         eventImage +
         "<h2>" + data[i].title + "</h2>" +
         "<p class='lead'>" + data[i].field_section + "</p>" +
