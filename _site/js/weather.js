11eda3215257cf2f8adce33f47f5a2fd69e58ee2
Nicholas Gunner <support@getbeyondwords.com>
2018-01-23 19:26:08 -0700

    working on the forecast

--- _site/js/weather.js
+++ _site/js/weather.js
@@ -4,9 +4,25 @@
   $.getJSON(currentWeatherAPI)
     .done(function( data ) {
       $( "#weather" ).append( 
+        "<div id='current'>" +
         "<h2>Currently</h2>" +
         "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>" +
-        "<p class='lead'>" + data.main.temp + " degrees</p>"
+        "<p class='lead'>" + data.main.temp + " degrees</p>" +
+        "</div><!-- #current -->"
      );
+    });
+})();
+
+// Get the current forcast
+(function() {
+  var currentWeatherAPI = "http://api.openweathermap.org/data/2.5/forecast?zip=14787,us&units=imperial&APPID=4e527f2dc589c215d247d03fc3b2263a";
+  $.getJSON(currentWeatherAPI)
+    .done(function( data ) {
+      $( "#weather" ).append( 
+        "<div id='forecast'>" +
+        "<h2>Forecast</h2>" +
+        "<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png'>" +
+        "</div><!-- #current -->"
+      );
     });
 })();
